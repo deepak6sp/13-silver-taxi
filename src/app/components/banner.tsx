bab450e9097788579dcbb7597fbda37fc22f63bd
Deepak Prakash <deepak@Deepaks-MBP.modem>
2024-09-08 00:53:57 +1000

    Change banner image

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import bannerPic from '../images/Taxi-waiting-for-a-pickup.jpg'
+import styles from '../styles/banner.module.css'
+
 const links = [
     { name: 'Book now', href: '#' },
     { name: 'Call us', href: '#' }
@@ -11,10 +15,10 @@ const stats = [
 
 const Banner = () => {
     return (
-        <section className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-            <img
+        <section className={`${styles.overlay} relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32`}>
+            <Image
                 alt=""
-                src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                src={bannerPic}
                 className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
             />
             <div

1dc3a0e129ee9c2cd12aa836e4c084eb61ceb220
Deepak Prakash <deepak@Deepaks-MBP.modem>
2024-09-10 20:36:39 +1000

    Update nav and banner

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -41,31 +41,31 @@ const callsToAction = [
 const Navbar = () => {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     return (
-        <header className="bg-white">
+        <header className="bg-slate-800">
             <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
                         <span className="sr-only">Your Company</span>
-                        <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
+                        <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=white" className="h-8 w-auto" />
                     </a>
                 </div>
                 <div className="flex lg:hidden">
                     <button
                         type="button"
                         onClick={() => setMobileMenuOpen(true)}
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
                     >
                         <span className="sr-only">Open main menu</span>
                         <Bars3Icon aria-hidden="true" className="h-6 w-6" />
                     </button>
                 </div>
                 <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Home
+                    <a href="#" className="text-sm font-bold leading-6 text-white">
+                        <strong>HOME</strong>
                     </a>
                     <Popover className="relative">
-                        <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-                            Services
+                        <PopoverButton className="flex items-center gap-x-1 text-sm font-bold leading-6 text-white">
+                            SERVICES
                             <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
                         </PopoverButton>
 
@@ -77,13 +77,13 @@ const Navbar = () => {
                                 {products.map((item) => (
                                     <div
                                         key={item.name}
-                                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-white hover:text-slate-800"
                                     >
                                         <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                                             <item.icon aria-hidden="true" className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" />
                                         </div>
                                         <div className="flex-auto">
-                                            <a href={item.href} className="block font-semibold text-gray-900">
+                                            <a href={item.href} className="block font-semibold text-white">
                                                 {item.name}
                                                 <span className="absolute inset-0" />
                                             </a>
@@ -97,7 +97,7 @@ const Navbar = () => {
                                     <a
                                         key={item.name}
                                         href={item.href}
-                                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-white hover:bg-gray-100"
                                     >
                                         <item.icon aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
                                         {item.name}
@@ -106,43 +106,43 @@ const Navbar = () => {
                             </div>
                         </PopoverPanel>
                     </Popover>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Booking
+                    <a href="#" className="text-sm font-bold leading-6 text-white">
+                        BOOKING
                     </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Contact us
+                    <a href="#" className="text-sm font-bold leading-6 text-white">
+                        CONTACT
                     </a>
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        About us
+                    <a href="#" className="text-sm font-bold leading-6 text-white">
+                        ABOUT
                     </a>
                 </PopoverGroup>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
                     <button
                         type="button"
-                        className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="inline-flex items-center rounded-md bg-rose-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                        <a href="#" className="text-sm font-semibold leading-6">
-                            Book now <span aria-hidden="true">&rarr;</span>
+                        <a href="#" className="text-sm font-bold leading-6">
+                            BOOK NOW <span aria-hidden="true">&rarr;</span>
                         </a>
                     </button>
                 </div>
             </nav>
             <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
                 <div className="fixed inset-0 z-10" />
-                <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-slate-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img
                                 alt=""
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                src="https://tailwindui.com/img/logos/mark.svg?color=white"
                                 className="h-8 w-auto"
                             />
                         </a>
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(false)}
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            className="-m-2.5 rounded-md p-2.5 text-white"
                         >
                             <span className="sr-only">Close menu</span>
                             <XMarkIcon aria-hidden="true" className="h-6 w-6" />
@@ -153,12 +153,12 @@ const Navbar = () => {
                             <div className="space-y-2 py-6">
                                 <a
                                     href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-sm font-semibold leading-7 text-white hover:bg-white hover:text-slate-800"
                                 >
-                                    Home
+                                    HOME
                                 </a>
                                 <Disclosure as="div" className="-mx-3">
-                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-sm font-semibold leading-7 text-white hover:bg-white hover:text-slate-800">
                                         Services
                                         <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none group-data-[open]:rotate-180" />
                                     </DisclosureButton>
@@ -168,7 +168,7 @@ const Navbar = () => {
                                                 key={item.name}
                                                 as="a"
                                                 href={item.href}
-                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                                className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-white hover:text-slate-800"
                                             >
                                                 {item.name}
                                             </DisclosureButton>
@@ -178,19 +178,19 @@ const Navbar = () => {
 
                                 <a
                                     href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-sm font-semibold leading-7 text-white hover:bg-white hover:text-slate-800"
                                 >
                                     Booking
                                 </a>
                                 <a
                                     href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-sm font-semibold leading-7 text-white hover:bg-white hover:text-slate-800"
                                 >
                                     Contact us
                                 </a>
                                 <a
                                     href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-sm font-semibold leading-7 text-white hover:bg-white hover:text-slate-800"
                                 >
                                     About us
                                 </a>
